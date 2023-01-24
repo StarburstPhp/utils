dcf331f215c439140112e5867a8ae98940427a1d
Andreas Sundqvist <andreas@stefna.is>
2023-01-24 09:56:38 +0100

    [utils] Add json helper
    It makes sure to add the correct flags to encode and decode.
    
    Also provide helper methods to get type safe arrays when decoding

---
+++ src/Json.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace Starburst\Utils;
+
+final class Json
+{
+	public static function encode(mixed $value): string
+	{
+		return json_encode($value, JSON_UNESCAPED_UNICODE|JSON_THROW_ON_ERROR|JSON_INVALID_UTF8_SUBSTITUTE);
+	}
+
+	public static function decode(string $value): mixed
+	{
+		return json_decode(
+			$value,
+			associative: true,
+			flags: JSON_UNESCAPED_UNICODE|JSON_THROW_ON_ERROR|JSON_INVALID_UTF8_SUBSTITUTE,
+		);
+	}
+
+	/**
+	 * @return array<string, mixed>
+	 */
+	public static function decodeArray(string $value): array
+	{
+		$output = self::decode($value);
+		if (!is_array($output)) {
+			throw new \JsonException('Output is not an array');
+		}
+		return $output;
+	}
+
+	/**
+	 * @return list<mixed>
+	 */
+	public static function decodeList(string $value): array
+	{
+		$output = self::decode($value);
+		if (!is_array($output) || !array_is_list($output)) {
+			throw new \JsonException('Output is not an list');
+		}
+		return $output;
+	}
+}
